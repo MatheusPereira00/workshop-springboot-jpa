19e63bad7609188c0f91c6f8e6fe54e811e15152
matheuspereira00 <matheusinhofcb12@gmail.com>
2023-11-06 12:59:47 -0300

    feat: add h2 database, test profile and jpa

--- src/main/java/com/matheus/workshop/Model/Users.java
+++ src/main/java/com/matheus/workshop/Model/Users.java
@@ -1,14 +1,20 @@
 package com.matheus.workshop.Model;
 
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "tb_user")
 public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
